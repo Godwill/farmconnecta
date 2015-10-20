b94b98e9f32321c106b2d882f343ce049dd56206
Godwill <kglikius@gmail.com>
2015-10-20 15:29:33 +0200

    Fixed bug on login token send

--- routes/users.js
+++ routes/users.js
@@ -10,6 +10,7 @@ passwordless.init(new RethinkDBStore({host: '127.0.0.1', port: 28015, db: 'farmc
 passwordless.addDelivery(
     function(tokenToSend, uidToSend, recipient, callback) {
         var message = "Access your FarmConnecta account here : http://farmconnecta.com/?token" + tokenToSend + '&uid=' + encodeURIComponent(uidToSend);
+        console.log(message);
         orangeAPI.sendSMS(recipient, message)
     });
 
